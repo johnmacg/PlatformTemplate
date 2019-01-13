f7029478a0e972ae597fdae8d37704bb59135c62
johnmacg <johnmacgalloway@mac.com>
2019-01-13 12:30:27 +0000

    Add files via upload
    Added dateParser

---
+++ dateParser.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+print "Current date and time function:"
+
+
+now = datetime.now()
+
+mm = str(now.month)
+
+dd = str(now.day)
+
+yyyy = str(now.year)
+
+hour = str(now.hour)
+
+mi = str(now.minute)
+
+ss = str(now.second)
+
+print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
